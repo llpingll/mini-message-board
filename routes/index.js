f47cb83bbb27f6495305261b9e20e6ccef5cff14
llpingll <94099281+llpingll@users.noreply.github.com>
2024-05-28 17:35:59 +0100

    Display messages on get method to index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+// Virtual database
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Message Board", messages: messages });
 });
 
 module.exports = router;
